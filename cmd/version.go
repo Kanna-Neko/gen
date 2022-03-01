77bdeb0a2b05142e7c7509a5b5fc6091aba15995
jaxleof <moonandxinxin@qq.com>
2022-03-01 23:06:09 +0800

    add version.go, now you can view version via gen version

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	gen.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of gen",
+	Long:  `All software has versions. This is gen's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gen version v0.8")
+	},
+}
